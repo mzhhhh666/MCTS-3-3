9b4bc4643c8efa8a303eecb2427c50da48da50e7
Hvestiny <1572032490@qqcom>
2025-09-03 10:39:15 +0800

    commit main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from state import state
 if __name__ == '__main__':
     current=[[1,-1,0],[1,0,-1],[0,0,0]]
     init_state=state(current)
-
-    mcts=mcts()
-    best_action,best_child=mcts.search(init_state)
+    mcts=mcts(uct_constant=1/math.sqrt(2))
+    best_action,best_child=mcts.search(init_state,iterations=1000)
     print(best_action)
     print(best_child.state.state)
 
